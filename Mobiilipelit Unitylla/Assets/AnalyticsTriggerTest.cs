b5a6e057745bfcd516492d24b052e232e52b2d4c
Olli <olli.juhola@careeria.fi>
2021-02-10 13:00:00 +0200

    analytiikka testi

---
+++ Mobiilipelit Unitylla/Assets/AnalyticsTriggerTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsTriggerTest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<AnalyticsFromCode>())
+        {
+            other.GetComponent<AnalyticsFromCode>().SendCustomEvent();
+        }
+    }
+}
---
+++ Mobiilipelit Unitylla/Assets/_Kurssipäivät/NeljäsKurssipäivä/Scripts/AnalyticsFromCode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsFromCode : MonoBehaviour
+{
+    public GameObject doorGO;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //// Toteuttaa saman asian kuin AnalyticsEventTracker -komponentit "AnalyticsEventTracker_Testi" GameObjektissa
+    //    AnalyticsEvent.GameStart();
+    //    AnalyticsEvent.LevelStart("StandardEventsTesti");
+    //}
+
+    public void SendCustomEvent()
+    {
+        Debug.Log("Lähetetään custom eventti");
+        doorGO.SetActive(true);
+
+        // key = Edellinen osio, value = uusi osio
+        Dictionary<string, object> customEventDictionary = new Dictionary<string, object>();
+        customEventDictionary.Add("osio 1", "osio 2");
+
+        Analytics.CustomEvent("osio_suoritettu", customEventDictionary);
+
+        Destroy(this.gameObject);
+    }
+
+    void OnDestroy()
+    {
+        // Toteuttaa saman asian kuin AnalyticsEventTracker -komponentit tässä GameObjektissa (inspektorissa)
+
+        AnalyticsEvent.LevelComplete("Osio 1");
+        AnalyticsEvent.LevelStart("Osio 2");
+    }
+}
